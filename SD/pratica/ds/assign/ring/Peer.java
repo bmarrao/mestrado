13d445319fe11d827551777d0fa9b61fc0e4f80c
bmarrao <marraob@gmail.com>
2023-10-11 17:59:35 +0100

    avanco trabalho PDE e SD

--- SD/pratica/ds/assign/ring/Peer.java
+++ SD/pratica/ds/assign/ring/Peer.java
@@ -1,4 +1,4 @@
-package ds.assign.ring;
+package ds.assing.ring;
 
 import java.io.IOException;
 import java.io.BufferedReader;
@@ -8,12 +8,14 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
+import java.util.concurrent.locks.Condition;
 import java.util.logging.Logger;
 import java.util.logging.FileHandler;
 import java.util.logging.SimpleFormatter;
 import java.util.Random;
 
-import ds.examples.sockets.poisson.PoissonProcess;
+import ds.assign.poisson.PoissonProcess;
+
 
 public class Peer {
     String host;
@@ -35,34 +37,70 @@ public class Peer {
     public static void main(String[] args) throws Exception {
 		Peer peer = new Peer(args[0]);
 		System.out.printf("new peer @ host=%s\n", args[0]);
-		new Thread(new Server(args[0], Integer.parseInt(args[1]), Integer.parseInt(arg[2]), peer.logger)).start();
-		new Thread(new Client(args[0], peer.logger)).start();
+        Lock lock = new ReentrantLock();
+        Condition tenhoToken = lock.newCondition();
+        String[] m = args[1].split(' ');
+        String[] m6 = args[2].split(' ');
+		new Thread(new Server("localhost", Integer.parseInt(args[1]),tenhoToken)).start();
+        new Thread(new Client("localhost",  m[0],Integer.parseInt(m[1]),m6[0],Integer.parseInt(m6[1]),tenhoToken,peer.logger)).start();
+		if (args.length == 4)
+		{
+			if (args[3].equals("first"))
+			{
+				tenhoToken.notify();
+			}
+		}
+
     }
 }
+
+
 
 class Server implements Runnable {
     String       host;
     int          port;
     ServerSocket server;
     Logger       logger;
+    Condition tenhoToken;
+	int flag;
     
-    public Server(String host, int port, Logger logger) throws Exception {
+    public Server(String host, int port, Logger logger, Condition  tenhoToken) throws Exception {
 	this.host   = host;
 	this.port   = port;
 	this.logger = logger;
+    this.tenhoToken = tenhoToken;
     server = new ServerSocket(port, 1, InetAddress.getByName(host));
     }
 
     @Override
-    public void run() {
-	try {
+    public void run() 
+    {
+	try 
+    {
 	    logger.info("server: endpoint running at port " + port + " ...");
 	    while(true) {
 		try {
 		    Socket client = server.accept();
 		    String clientAddress = client.getInetAddress().getHostAddress();
 		    logger.info("server: new connection from " + clientAddress);
-		    new Thread(new Connection(clientAddress, client, logger)).start();
+            try
+            {
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));    
+	            PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+                String command;
+                command = in.readLine();
+                logger.info("server: message from host " + clientAddress + "[command = " + command + "]");
+				if (command.equals("token"))
+				{
+					tenhoToken.notify();
+				}
+
+
+            }
+            catch(Exception e) 
+            {
+	            e.printStackTrace();
+	        }
 		}catch(Exception e) {
 		    e.printStackTrace();
 		}    
@@ -73,87 +111,43 @@ class Server implements Runnable {
     }
 }
 
-class Connection implements Runnable {
-    String clientAddress;
-    Socket clientSocket;
-    Logger logger;
 
-    public Connection(String clientAddress, Socket clientSocket, Logger logger) {
-	this.clientAddress = clientAddress;
-	this.clientSocket  = clientSocket;
-	this.logger        = logger;
-    }
 
-    @Override
-    public void run() {
-	/*
-	 * prepare socket I/O channels
-	 */
-	try {
-	    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));    
-	    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-	
-	    String command;
-	    command = in.readLine();
-	    logger.info("server: message from host " + clientAddress + "[command = " + command + "]");
-	    /*
-	     * parse command
-	     */
-	    Scanner sc = new Scanner(command);
-	    String  op = sc.next();	    
-	    double  x  = Double.parseDouble(sc.next());
-	    double  y  = Double.parseDouble(sc.next());
-	    double  result = 0.0; 
-	    /*
-	     * execute op
-	     */
-	    switch(op) {
-	    case "add": result = x + y; break;
-	    case "sub": result = x - y; break;
-	    case "mul": result = x * y; break;
-	    case "div": result = x / y; break;
-	    }  
-	    /*
-	     * send result
-	     */
-	    out.println(String.valueOf(result));
-	    out.flush();
-	    /*
-	     * close connection
-	     */
-	    clientSocket.close();
-	} catch(Exception e) {
-	    e.printStackTrace();
-	}
-    }
-}
-
-class Client implements Runnable {
+class Client implements Runnable 
+{
     String  host;
+	String Mip;
+	String M6ip;
+	Int Mport;
+	Int M6port;
+	Condition tenhoToken;
     Logger  logger;
     Scanner scanner;
 	PoissonProcess pp;
     
-    public Client(String host, Logger logger) throws Exception {
+
+	//	new Thread(new Client("localhost",  m[0],Integer.parseInt(m[1]),m6[0],Integer.parseInt(m6[1]),tenhoToken,peer.logger)).start();
+
+    public Client(String host, String Mip , Int Mport , String M6ip , Int M6porto,Condition tenhoToken, Logger logger) throws Exception 
+	{
 		this.host    = host;
 		this.logger  = logger; 
-		this.scanner = new Scanner(System.in);
-		this.pp = new PoissonProcess(5, new Random(0));
+		this.Mip = Mip ;
+		this.Mport = Mport;
+		this.M6ip = M6ip;
+		this.M6ip = M6ip;
+		this.tenhoToken= tenhoToken;
+		this.logger = logger;
+		this.pp = new PoissonProcess(4, new Random(0));
 
     }
 
     @Override 
-    public void run() {
-	try {
+    public void run() 
+	{
+	try 
+	{
 	    logger.info("client: endpoint running ...\n");	
-	    /*
-	     * send messages such as:
-	     *   - ip port add x y
-	     *   - ip port sub x y
-	     *   - ip port mul x y
-	     *   - ip port div x y
-	     * where x, y are floting point values and ip is the address of the server
-	     */
 
 		int t;
 		Random rand = new Random();
@@ -161,13 +155,18 @@ class Client implements Runnable {
 		int x;
 		int y;
 		String[]operations = {"add","sub","mul","div"};
-	    while (true) {
-		try {
+	    while (true) 
+		{
+		try 
+		{
+			
 
-			try{
+			try
+			{
 				t = (int)(this.pp.timeForNextEvent()*60.0*1000.0);
 				Thread.sleep(t);
-				Socket socket  = new Socket(InetAddress.getByName("localhost"), 3000);
+				tenhoToken.await();
+				Socket socket  = new Socket(InetAddress.getByName(M6ip), M6port);
 				logger.info("client: connected to server " + socket.getInetAddress() + "[port = " + socket.getPort() + "]");
 
 				PrintWriter   out = new PrintWriter(socket.getOutputStream(), true);
@@ -181,31 +180,16 @@ class Client implements Runnable {
 				String result = in.readLine();
 				System.out.printf(result +"\n");
 				socket.close();
+
+				socket  = new Socket(InetAddress.getByName(Mip), Mport);
+				out = new PrintWriter(socket.getOutputStream(), true);
+				in = new BufferedReader(new InputStreamReader(socket.getInputStream())); 
+				out.println("token\n");
+				out.flush();
 			}
 			catch(Exception e) {
 		    e.printStackTrace();
-			}   
-			/*
-
-		    System.out.print("$ ");
-		    String server  = scanner.next();
-		    String port    = scanner.next();
-		    String command = scanner.nextLine();
-
-		    Socket socket  = new Socket(InetAddress.getByName(server), Integer.parseInt(port));
-		    logger.info("client: connected to server " + socket.getInetAddress() + "[port = " + socket.getPort() + "]");
-
-		    PrintWriter   out = new PrintWriter(socket.getOutputStream(), true);
-		    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));    
-
-		    out.println(command);
-		    out.flush();	    
-
-		    String result = in.readLine();
-						//System.out.printf("" + this.pp.events());
-
-		    socket.close();
-			*/
+			}   lose();
 
 		} catch(Exception e) {
 		    e.printStackTrace();
